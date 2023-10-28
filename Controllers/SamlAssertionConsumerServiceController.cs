5c4236aa1ff046f5c77884c93cbc378b1ed7cfe1
Jonnel Necio <jonnel.necio@gmail.com>
2023-10-27 19:04:42 -0500

    Add project files.

---
+++ Controllers/SamlAssertionConsumerServiceController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.IdentityModel.Tokens.Saml2;
+//using Microsoft.IdentityModel.Tokens.Saml2;
+//using Sustainsys.Saml2.Tokens;
+using System.Xml;
+
+namespace RaadTestSSO.Controllers
+{
+    public class SamlAssertionConsumerServiceController : Controller
+    {
+        /*[HttpPost]
+        public IActionResult Post()
+        {
+            // Handle and process the incoming SAML assertion here
+            // Validate the assertion, create or update the user session, and perform application-specific actions
+
+            // Return a response as needed
+
+            return View();
+        }*/
+
+        [HttpPost]
+        [Route("saml/AssertionConsumer")]
+        public ActionResult SamlAcs()
+        {
+            // Get the SAML Response from the form data
+            string samlResponse = Request.Form["SAMLResponse"];
+
+            // Process and validate the SAML response
+            try
+            {
+                // Load the SAML response into an XML document
+                XmlDocument samlResponseXml = new XmlDocument();
+                samlResponseXml.LoadXml(samlResponse);
+
+                // Perform SAML response validation, such as checking the signature
+                // You may need to configure your SAML library for this step
+
+                // Extract the SAML assertion
+                // TODO: SecurityToken samlAssertion = /* Extract and validate the SAML assertion here */;
+
+                // Log the user in or perform further processing
+                // Typically, you would extract user information from the SAML assertion
+                // and use it for authentication and authorization.
+
+                // Example: You can access user attributes from the SAML assertion
+                // TODO: string? username = samlAssertion as Saml2SecurityToken;
+
+                // Example: Log the user in
+                //FormsAuthentication.SetAuthCookie(username, false);
+
+                // Redirect to a post-login landing page
+                return RedirectToAction("Welcome", "Home");
+            }
+            catch (Exception ex)
+            {
+                // Handle SAML response validation or processing errors
+                // You should implement proper error handling and logging here.
+                return View("Error");
+            }
+        }
+
+        [HttpGet]
+        [Route("saml/Login")]
+        public ActionResult SamlLogin()
+        {
+            try
+            {
+
+                // Redirect to a post-login landing page
+                return RedirectToAction("Welcome", "Home");
+            }
+            catch (Exception ex)
+            {
+                // Handle SAML response validation or processing errors
+                // You should implement proper error handling and logging here.
+                return View("Error");
+            }
+        }
+
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
+using Sustainsys.Saml2.Metadata;
+using IHostingEnvironment = Microsoft.Extensions.Hosting.IHostingEnvironment;
+
+namespace RaadTestSSO
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // ... other configurations
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultSignOutScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = "SAML"; // Your SAML authentication scheme
+            })
+            .AddCookie()
+            .AddSaml2("SAML", options =>
+            {
+                // Configure your SAML options here, including the ACS URL.
+                options.SPOptions.EntityId = new EntityId("RaadSamlToolKit");
+                options.SPOptions.ReturnUrl = new Uri("https://jonneldemosite.azurewebsites.net/SAML/AssertionConsumerService");
+                // ... other SAML options
+            });
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            // ... other middleware configurations
+
+            app.UseAuthentication();
+
+
+            var builder = WebApplication.CreateBuilder();
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            var app2 = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app2.Environment.IsDevelopment())
+            {
+                app2.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app2.UseHsts();
+            }
+
+            app2.UseHttpsRedirection();
+            app2.UseStaticFiles();
+
+            app2.UseRouting();
+
+            app2.UseAuthorization();
+
+            app2.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app2.Run();
+
+
+        }
+
+    }
+}
